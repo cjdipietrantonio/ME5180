b049449a01caed44d7a7729cd3853cdeca2dd9c0
Christian DiPietrantonio <cjdipietrantonio@gmail.com>
2026-02-06 19:27:29 -0500

    adding notebook

---
+++ discussions/discussion03/discussion03.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v0.20.21
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 2c9a8732-03b3-11f1-b91f-fdd270b86be6
+include("06_least-action.jl")
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.4"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═2c9a8732-03b3-11f1-b91f-fdd270b86be6
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
